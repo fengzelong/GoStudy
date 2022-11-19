4fccc5f49aac966ef13da7cc9045ef4c2e8371dc
feng long <taichipanda648@gmail.com>
2022-11-19 12:27:34 +0800

    debug grpc service

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	pb "GoStudy/proto/grpc"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+)
+
+const (
+	// Address gRPC服务地址
+	Address = "127.0.0.1:50052"
+)
+
+func main() {
+	// 连接
+	conn, err := grpc.Dial(Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	defer conn.Close()
+
+	// 初始化客户端
+	client := pb.NewGrpcClient(conn)
+
+	// 调用方法
+	//req := &pb.HelloRequest{Name: "lily", Sex: "female"}
+	//res, err1 := c.SayHello(context.Background(), req)
+	//if err1 != nil {
+	//	grpclog.Fatalln(err1)
+	//}
+	//fmt.Println(res.Message)
+
+	var idx int32 = 0
+	ctx := context.Background()
+	for idx < 50 {
+		idx++
+		req := &pb.SumRequest{A: idx * 2, B: (idx * 2) + 1}
+		res, err1 := client.SumFunc(ctx, req)
+		if err1 != nil {
+			grpclog.Fatalln(err1)
+		}
+		fmt.Println(res.Total)
+		//time.Sleep(1 * time.Second)
+	}
+}
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	pb "GoStudy/proto/grpc"
+	"fmt"
+	"golang.org/x/net/context"
+	"golang.org/x/net/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 	"net"
+	"net/http"
 )
 
 const (
@@ -11,32 +16,52 @@ const (
 	Address = "127.0.0.1:50052"
 )
 
-// 定义helloService并实现约定的接口
-type helloService struct{}
+// GrpcService 实现约定的接口
+type GrpcService struct{}
 
-// HelloService Hello服务
-var HelloService = helloService{}
+// SayHello grpc服务go实现
+func (g GrpcService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	resp := new(pb.HelloResponse)
+	resp.Message = fmt.Sprintf("Hello %s, your sex: %s", in.Name, in.Sex)
 
-//func SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
-//	resp := new(pb.HelloResponse)
-//	resp.Message = fmt.Sprintf("Hello %s.", in.Name)
-//
-//	return resp, nil
-//}
+	return resp, nil
+}
+
+// SumFunc grpc服务go实现
+func (g GrpcService) SumFunc(ctx context.Context, in *pb.SumRequest) (*pb.SumResponse, error) {
+	resp := new(pb.SumResponse)
+	total := in.A + in.B
+	resp.Total = total
+	return resp, nil
+}
+
+// GrpcSvc Hello服务
+var GrpcSvc = GrpcService{}
 
 func main() {
-	//fmt.Println("git commit")
 	listen, err := net.Listen("tcp", Address)
 	if err != nil {
 		grpclog.Fatalf("Failed to listen: %v", err)
 	}
 
 	// 实例化grpc Server
-	s := grpc.NewServer()
+	svc := grpc.NewServer()
 
 	// 注册HelloService
-	//pb.RegisterHelloServer(s, HelloService)
+	pb.RegisterGrpcServer(svc, GrpcSvc)
 
-	grpclog.Println("Listen on " + Address)
-	s.Serve(listen)
+	// 开启trace
+	go startTrace()
+
+	fmt.Println("Listen on " + Address)
+	svc.Serve(listen)
+}
+
+// startTrace trace
+func startTrace() {
+	trace.AuthRequest = func(req *http.Request) (any, sensitive bool) {
+		return true, true
+	}
+	go http.ListenAndServe(":50051", nil)
+	fmt.Println("Trace listen on 50051")
 }
